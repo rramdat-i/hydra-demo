84e8052fd526eb259220e134b98fb5d94499c2dc
Richard <rramdat@infusion.com>
2013-04-04 23:53:37 +0000

    initial checkin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HydraDemo::Application.config.secret_token = '32f8932f3dcc11cb63d36eddfb0be2d78032fc9b402fb6298c4bae025febda023c6f0e02030957d3fb58fe017967450a6fa3c477b91966a186986366a42d2823'
