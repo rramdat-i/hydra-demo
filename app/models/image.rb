2504b041a0538b376db41d75679913a7e3a79734
Richard <rramdat@infusion.com>
2013-04-15 04:44:34 +0000

    update for sites scaffolding.

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveFedora::Base
   has_metadata 'descMetadata', type: Datastream::ImageMetadata
   has_file_datastream 'imageContent'
  
-  belongs_to: :site, :property=> :is_part_of
+  belongs_to :site, :property=> :is_part_of
 
   delegate :caption, to: 'descMetadata'
   delegate :photograph_date, to: 'descMetadata'
